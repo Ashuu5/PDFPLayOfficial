8d4ca0f443604aa870ea877206e5fc9271a90426
Ashuu5 <pdfplayofficial@gmail.com>
2026-09-26 00:00:58 +0500

    Fix some changes

--- src/App.tsx
+++ src/App.tsx
@@ -29,9 +29,6 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
 import Contact from './pages/Contact';
 
-// ✅ Audio to Text tool
-import AudioToText from './pages/AudioToText';
-
 function App() {
   return (
     <Router>
@@ -67,9 +64,6 @@ function App() {
           <Route path="/ocr-pdf" element={<OCRPDF />} />
           <Route path="/crop-pdf" element={<CropPDF />} />
           <Route path="/metadata-editor" element={<MetadataEditor />} />
-
-          {/* ✅ Audio to Text tool */}
-          <Route path="/audio-to-text" element={<AudioToText />} />
         </Routes>
       </Layout>
     </Router>
